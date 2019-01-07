f308bceabedb5786d040832cb638dabb37126797
kedar giri <giri.kedar984@outlook.com>
2019-01-07 21:53:32 +0545

    THIRD COMMIT OPEN FILE DIALOGUE

--- GraphicalProgrammingLanguage/Form1.Designer.cs
+++ GraphicalProgrammingLanguage/Form1.Designer.cs
@@ -29,98 +29,91 @@
         private void InitializeComponent()
         {
             this.panel2 = new System.Windows.Forms.Panel();
-            this.button5 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.txtLoadFile = new System.Windows.Forms.TextBox();
+            this.btnopen = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panel2.SuspendLayout();
             this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // panel2
             // 
             this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.txtLoadFile);
             this.panel2.Controls.Add(this.panel1);
             this.panel2.Controls.Add(this.button3);
-            this.panel2.Controls.Add(this.textBox1);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel2.Location = new System.Drawing.Point(0, 0);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(911, 212);
+            this.panel2.Size = new System.Drawing.Size(911, 231);
             this.panel2.TabIndex = 3;
-            // 
-            // button5
-            // 
-            this.button5.Location = new System.Drawing.Point(24, 111);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(134, 51);
-            this.button5.TabIndex = 5;
-            this.button5.Text = "Browse";
-            this.button5.UseVisualStyleBackColor = true;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(3, 3);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(343, 113);
-            this.textBox1.TabIndex = 9;
             // 
             // panel1
             // 
             this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panel1.Controls.Add(this.textBox2);
-            this.panel1.Controls.Add(this.button5);
-            this.panel1.Controls.Add(this.button1);
-            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.btnopen);
+            this.panel1.Controls.Add(this.btnSave);
             this.panel1.Location = new System.Drawing.Point(367, 12);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(531, 183);
             this.panel1.TabIndex = 10;
             // 
-            // textBox2
+            // txtLoadFile
             // 
-            this.textBox2.Location = new System.Drawing.Point(5, 6);
-            this.textBox2.Multiline = true;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(343, 37);
-            this.textBox2.TabIndex = 16;
+            this.txtLoadFile.Location = new System.Drawing.Point(3, 11);
+            this.txtLoadFile.Multiline = true;
+            this.txtLoadFile.Name = "txtLoadFile";
+            this.txtLoadFile.Size = new System.Drawing.Size(358, 127);
+            this.txtLoadFile.TabIndex = 16;
+            // 
+            // btnopen
+            // 
+            this.btnopen.Location = new System.Drawing.Point(341, 111);
+            this.btnopen.Name = "btnopen";
+            this.btnopen.Size = new System.Drawing.Size(162, 51);
+            this.btnopen.TabIndex = 14;
+            this.btnopen.Text = "Open";
+            this.btnopen.UseVisualStyleBackColor = true;
+            this.btnopen.Click += new System.EventHandler(this.btnopen_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(173, 111);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(162, 51);
+            this.btnSave.TabIndex = 13;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(11, 124);
+            this.button3.Location = new System.Drawing.Point(3, 144);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(162, 51);
             this.button3.TabIndex = 15;
             this.button3.Text = "Run";
             this.button3.UseVisualStyleBackColor = true;
             // 
-            // button1
+            // pictureBox1
             // 
-            this.button1.Location = new System.Drawing.Point(341, 111);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(162, 51);
-            this.button1.TabIndex = 14;
-            this.button1.Text = "Browse";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(173, 111);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(162, 51);
-            this.button2.TabIndex = 13;
-            this.button2.Text = "Save";
-            this.button2.UseVisualStyleBackColor = true;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 231);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(911, 420);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(911, 488);
+            this.ClientSize = new System.Drawing.Size(911, 651);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.panel2);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -128,7 +121,7 @@
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
             this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -136,12 +129,11 @@
         #endregion
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtLoadFile;
+        private System.Windows.Forms.Button btnopen;
+        private System.Windows.Forms.Button btnSave;
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- GraphicalProgrammingLanguage/Form1.cs
+++ GraphicalProgrammingLanguage/Form1.cs
@@ -6,7 +6,10 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+//using System.Text;
+using System.IO;
 using System.Windows.Forms;
+using Microsoft.Office.Interop.Word;
 
 namespace GraphicalProgrammingLanguage
 {
@@ -21,5 +24,126 @@ namespace GraphicalProgrammingLanguage
         {
 
         }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "Open File You Want TO Open";
+            //string text = File.ReadAllText(file);
+            // ofd.Filter = "All files (*.*)|*.*|All files (*.*)|*.*";
+            // ofd.Filter = "BackUp File(*.bak)|*.bak|All Files(*.*)|(*.*)";
+           ofd.Filter = "All Files (*.docx;*.Pdf;*.txt;)|*.docx;*.Pdf;*.txt;";
+            //           (*.BMP; *.JPG; *.GIF,*.PNG,*.TIFF)| *.BMP; *.JPG; *.GIF; *.PNG; *.TIFF | " +  
+            //"All files (*.*)|*.*";
+            ofd.FilterIndex = 0;
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                txtLoadFile.Text = ofd.FileName;
+            }
+        
+    }
+
+        private void btnopen_Click(object sender, EventArgs e)
+        {
+            //Stream mystream;
+            //OpenFileDialog ofd = new OpenFileDialog();
+            //ofd.Title = "Open File you want to Open";
+            //ofd.Filter = "All Files (*.docx;*.Pdf;*.txt;)|*.docx;*.Pdf;*.txt;";
+            //if (ofd.ShowDialog()==System.Windows.Forms.DialogResult.OK)
+            //{
+            //    if((mystream=ofd.OpenFile())!=null)
+            //    {
+
+            //        string kedar = ofd.FileName;
+            //        string filetextupload = File.ReadAllText(kedar);
+            //        txtrun.Text = filetextupload;
+            //    }
+            //}
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Word Documents|*.doc; *.docx;*.txt;*.pdf";
+
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                Microsoft.Office.Interop.Word.Application word = new Microsoft.Office.Interop.Word.Application();
+                object miss = System.Reflection.Missing.Value;
+                object path = ofd.FileName;
+                object readOnly = true;
+                Microsoft.Office.Interop.Word.Document docs = word.Documents.Open(ref path, ref miss, ref readOnly,
+                    ref miss, ref miss, ref miss, ref miss, ref miss, ref miss, ref miss, ref miss, ref miss,
+                    ref miss, ref miss, ref miss, ref miss);
+                var totaltextBuilder = new StringBuilder();
+
+                for (int i = 0; i < docs.Paragraphs.Count; i++)
+                {
+                    totaltextBuilder.Append( docs.Paragraphs[i + 1].Range.Text.ToString());
+                }
+
+               txtLoadFile.Text = totaltextBuilder.ToString();
+            }
+
+
+
+
+
+
+
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            Stream myStream;
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+
+            saveFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            saveFileDialog1.FilterIndex = 2;
+            saveFileDialog1.RestoreDirectory = true;
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                if ((myStream = saveFileDialog1.OpenFile()) != null)
+                {
+                    // Code to write the stream goes here.
+                    myStream.Close();
+
+                }
+
+
+                    //// Displays a SaveFileDialog so the user can save the Image  
+                    //// assigned to Button2.  
+                    //SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+                    //saveFileDialog1.Filter = "JPeg Image|*.jpg|Bitmap Image|*.bmp|Gif Image|*.gif";
+                    //saveFileDialog1.Title = "Save an Image File";
+                    //saveFileDialog1.ShowDialog();
+
+                    //// If the file name is not an empty string open it for saving.  
+                    //if (saveFileDialog1.FileName != "")
+                    //{
+                    //    // Saves the Image via a FileStream created by the OpenFile method.  
+                    //    System.IO.FileStream fs =
+                    //       (System.IO.FileStream)saveFileDialog1.OpenFile();
+                    //    // Saves the Image in the appropriate ImageFormat based upon the  
+                    //    // File type selected in the dialog box.  
+                    //    // NOTE that the FilterIndex property is one-based.  
+                    //    switch (saveFileDialog1.FilterIndex)
+                    //    {
+                    //        case 1:
+                    //            this.btnSave.Image.Save(fs,
+                    //               System.Drawing.Imaging.ImageFormat.Jpeg);
+                    //            break;
+
+                    //        case 2:
+                    //            this.btnSave.Image.Save(fs,
+                    //               System.Drawing.Imaging.ImageFormat.Bmp);
+                    //            break;
+
+                    //        case 3:
+                    //            this.btnSave.Image.Save(fs,
+                    //               System.Drawing.Imaging.ImageFormat.Gif);
+                    //            break;
+                    //    }
+
+                    //    fs.Close();
+                }
+        }  
     }
 }
